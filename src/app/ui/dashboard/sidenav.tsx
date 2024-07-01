7cdfde664db3f6d2016bddf4a60556ad47cb1356
Marco Rossel <andresmarcorossel@gmail.com>
2024-07-01 04:51:06 -0400

    next app/folder | 10 | loading ...

--- src/app/ui/dashboard/sidenav.tsx
+++ src/app/ui/dashboard/sidenav.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link" // reemplazamos a href por Link href y no se recarga la pagina a navegar por los links
 import styles from "./sidenav.module.css"
 
 export default function SideNav() {
@@ -5,8 +6,10 @@ export default function SideNav() {
     <div className={styles.side_nav}>
       <p>sidenav</p>
       <p>todo lo que coloque en este div sera perpetuo en los siguientes url's</p>
-      <a href="http://localhost:3000/dashboard/invoices">invoices</a>
-      <a href="http://localhost:3000/dashboard/customers">customers</a>
+      <Link href="http://localhost:3000/dashboard/invoices"><p>invoices</p> </ Link >
+      <Link href="http://localhost:3000/dashboard/customers"><p>customers</p> </ Link >
+      <Link href="http://localhost:3000"><p>home</p> </ Link >
+      <Link href="http://localhost:3000/characters"><p>rick and morty data</p> </ Link >
     </div>
   )
 }
