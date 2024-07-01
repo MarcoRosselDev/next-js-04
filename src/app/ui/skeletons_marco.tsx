7b2a8fd610d55af931e5e6ae430f60c17debb062
Marco Rossel <andresmarcorossel@gmail.com>
2024-07-01 10:55:20 -0400

    next app/folder | 11 | skeleton

---
+++ src/app/ui/skeletons_marco.tsx
@@ -0,0 +1,8 @@
+export function DashboardSkeleton() {
+  return (
+    <div>
+      <p>esto se carga en reemplazo de la pagina si no se a cargado ahun</p>
+      <p>se agregan stylos tmbn</p>
+    </div>
+  )
+}
