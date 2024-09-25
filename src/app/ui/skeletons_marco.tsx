91cd8bc7ed91286d53e1fa4e36b84463d9891774
Marco Rossel <andresmarcorossel@gmail.com>
2024-09-25 01:07:53 -0300

    nx-4 | learn reminder

--- src/app/ui/skeletons_marco.tsx
+++
@@ -1,8 +0,0 @@
-export function DashboardSkeleton() {
-  return (
-    <div>
-      <p>esto se carga en reemplazo de la pagina si no se a cargado ahun</p>
-      <p>se agregan stylos tmbn</p>
-    </div>
-  )
-}
